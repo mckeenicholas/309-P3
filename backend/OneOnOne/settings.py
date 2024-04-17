2779687939665c9ddab60137b3156abe7e8638bd
Nicholas McKee <nmsmckee@gmail.com>
2024-04-17 16:29:41 -0400

    fix

--- backend/OneOnOne/settings.py
+++ backend/OneOnOne/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from OneOnOne import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'oneononeautoemail@gmail.com'
-EMAIL_HOST_PASSWORD = 'ONEONONE123'
+EMAIL_HOST_USER = secrets.EMAIL_ADDRESS
+EMAIL_HOST_PASSWORD = secrets.EMAIL_PASSWORD
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
